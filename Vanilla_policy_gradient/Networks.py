dfa9e7ccbc9a07862a93432ed6bb79ff3ffe327e
dshekhar <dshekhar@tamu.edu>
2023-04-08 12:30:49 -0500

    <1> Vanilla Policy Gradient, <2> Actor-crtic renaming

---
+++ Vanilla_policy_gradient/Networks.py
@@ -0,0 +1,30 @@
+import os
+import torch
+import torch.nn as nn
+
+class MLPModel(nn.Module):
+    def __init__(self, n_actions, fc1_dims = 1024, fc2_dims = 512, model_name = "MLP_model", chkpt_dir = "checkpoints/"):
+        super(MLPModel, self).__init__()
+        self.fc1_dims   = fc1_dims
+        self.fc2_dims   = fc2_dims
+        self.n_actions  = n_actions
+        self.model_name = model_name
+        self.chkpt_dir  = chkpt_dir
+        self.checkpoint_file = os.path.join(self.chkpt_dir, model_name+'_ac')
+        
+        self.fc1  = nn.LazyLinear(out_features = self.fc1_dims)
+        self.fc2  = nn.LazyLinear(out_features = self.fc2_dims)
+        self.relu = nn.ReLU()
+        self.pi   = nn.LazyLinear(out_features = self.n_actions)
+        self.softmax = nn.Softmax()
+        
+    def forward(self, observation):
+        out = self.fc1(observation)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        pi  = self.pi(out)
+        pi  = self.softmax(pi)
+        
+        return pi
+        
---
+++ Vanilla_policy_gradient/main.py
@@ -0,0 +1,78 @@
+import gym
+import numpy as np
+from Agent import Agent
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v0')
+    agent = Agent(alpha=1e-3, n_actions=env.action_space.n)
+    n_games = 250
+    batch_size = 5000
+
+    filename = 'cartpole.png'
+    figure_file = 'plots/' + filename
+
+    best_score = env.reward_range[0]
+    score_history = []
+
+    render = False
+    load_checkpoint = False
+
+    if load_checkpoint:
+        agent.load_models()
+
+    for i in range(n_games):
+        batch_observation = []
+        batch_actions     = []
+        batch_weights     = []
+        batch_returns     = []
+        batch_lens        = []
+        
+        observation = env.reset()
+        done = False
+        ep_rewards = []
+        
+        finished_rendering_this_epoch = False
+        
+        score = 0
+        while True:
+            if (not finished_rendering_this_epoch) and render:
+                env.render()
+
+            batch_observation.append(observation.copy())
+            action = agent.choose_action(observation)
+            new_observation, reward, done, info = env.step(action)
+
+            batch_actions.append(action)
+            ep_rewards.append(reward)
+
+            if done:
+                ep_return, ep_length = sum(ep_rewards), len(ep_rewards)
+                batch_returns.append(ep_return)
+                batch_lens.append(ep_length)
+
+                batch_weights += [ep_return] * ep_length
+
+                observation, done, ep_rewards = env.reset(), False, []
+
+                finished_rendering_this_epoch = True
+                if len(batch_observation) > batch_size:
+                        break
+        
+        agent.learn(batch_states=batch_observation, batch_actions=batch_actions, batch_weights=batch_weights)
+
+        avg_score = np.mean(batch_returns[-100:])
+        print(avg_score)
+        score_history.append(avg_score)
+
+        if avg_score > best_score:
+            best_score = avg_score
+            if not load_checkpoint:
+                agent.save_models()
+
+    games = [i for i in range(n_games)]
+    plt.plot(games, score_history)
+    plt.title("Score plot")
+    plt.xlabel("Game number")
+    plt.ylabel("Score")
+    plt.savefig("results/plot.png")
