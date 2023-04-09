Softmax fix, correct rewards retrieval

Simple_policy_gradient/Networks.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 
 class MLPModel(nn.Module):
-    def __init__(self, n_actions, fc1_dims = 1024, fc2_dims = 512, model_name = "MLP_model", chkpt_dir = "checkpoints/"):
+    def __init__(self, n_actions, fc1_dims = 64, fc2_dims = 32, model_name = "MLP_model", chkpt_dir = "checkpoints/"):
         super(MLPModel, self).__init__()
         self.fc1_dims   = fc1_dims
         self.fc2_dims   = fc2_dims
@@ -16,7 +16,7 @@ class MLPModel(nn.Module):
         self.fc2  = nn.LazyLinear(out_features = self.fc2_dims)
         self.relu = nn.ReLU()
         self.pi   = nn.LazyLinear(out_features = self.n_actions)
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim = -1)
         
     def forward(self, observation):
         out = self.fc1(observation)
@@ -27,4 +27,3 @@ class MLPModel(nn.Module):
         pi  = self.softmax(pi)
         
         return pi
-        

Simple_policy_gradient/main.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     env = gym.make('CartPole-v0')
-    agent = Agent(alpha=1e-3, n_actions=env.action_space.n)
-    n_games = 250
+    agent = Agent(alpha=1e-2, n_actions=env.action_space.n)
+    n_games = 50
     batch_size = 5000
 
     filename = 'cartpole.png'
@@ -34,14 +34,13 @@ if __name__ == '__main__':
         
         finished_rendering_this_epoch = False
         
-        score = 0
         while True:
             if (not finished_rendering_this_epoch) and render:
                 env.render()
 
             batch_observation.append(observation.copy())
             action = agent.choose_action(observation)
-            new_observation, reward, done, info = env.step(action)
+            observation, reward, done, info = env.step(action)
 
             batch_actions.append(action)
             ep_rewards.append(reward)
@@ -59,10 +58,10 @@ if __name__ == '__main__':
                 if len(batch_observation) > batch_size:
                         break
         
-        agent.learn(batch_states=batch_observation, batch_actions=batch_actions, batch_weights=batch_weights)
+        batch_loss = agent.learn(batch_states=batch_observation, batch_actions=batch_actions, batch_weights=batch_weights)
 
-        avg_score = np.mean(batch_returns[-100:])
-        print(avg_score)
+        avg_score = np.mean(batch_returns)
+        print("Epoch number: {}, Loss : {}, Average_returns: {}".format(i, batch_loss.item(), avg_score))
         score_history.append(avg_score)
 
         if avg_score > best_score: