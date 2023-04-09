e584f7e6da68bb81512257c2d909a8643bf31e60
dshekhar <dshekhar@tamu.edu>
2023-04-08 21:14:22 -0500

    Simple policy gradient using returns togo not full episode/trajectory reward

---
+++ Simple_policy_gradient_rtg/Agent.py
@@ -0,0 +1,53 @@
+
+import os
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.distributions.categorical import Categorical
+from Networks import MLPModel
+
+class Agent:
+    def __init__(self, alpha=0.0001, n_actions = 2):
+        self.n_actions = n_actions
+        self.action = None
+        self.action_space = [i for i in range(self.n_actions)]
+
+        self.MLPModel  = MLPModel(n_actions = self.n_actions)
+        self.optimizer = Adam(self.MLPModel.parameters(), lr = alpha)
+
+    def choose_action(self, observation):
+        state = torch.Tensor([observation])
+        probs = self.MLPModel(state)
+
+        action_probs = Categorical(probs=probs) # distribution of action space
+        action = action_probs.sample() # action sampling
+        self.action = action
+
+        return self.action.numpy()[0]
+    
+    def save_models(self):
+        print("...Saving models...")
+        torch.save(self.MLPModel.state_dict, self.MLPModel.checkpoint_file)
+
+    def load_models(self):
+        print("..Loading models...")
+        self.MLPModel.load_state_dict(self.MLPModel.checkpoint_file)
+
+    def learn(self, batch_states, batch_actions, batch_weights):
+        state  = torch.Tensor([batch_states])
+        action = torch.Tensor([batch_actions])
+        reward = torch.Tensor([batch_weights])
+
+        probs = self.MLPModel(state)
+        action_probs = Categorical(probs=probs)
+        logp = action_probs.log_prob(action)
+        # print(torch.unique(logp))
+        # print(probs.shape)
+        # print("shape of logp: {}, probs : {}, rewards : {}".format(logp.shape, probs.shape, reward.shape))
+        self.optimizer.zero_grad()
+        loss = -(logp * reward).mean()
+
+        loss.backward()
+        self.optimizer.step()
+        
+        return loss
---
+++ Simple_policy_gradient_rtg/main.py
@@ -0,0 +1,79 @@
+import gym
+import numpy as np
+from Agent import Agent
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v0')
+    agent = Agent(alpha=1e-2, n_actions=env.action_space.n)
+    n_games = 50
+    batch_size = 5000
+
+    filename = 'cartpole.png'
+    figure_file = 'plots/' + filename
+
+    best_score = env.reward_range[0]
+    score_history = []
+
+    render = False
+    load_checkpoint = False
+
+    if load_checkpoint:
+        agent.load_models()
+
+    for i in range(n_games):
+        batch_observation = []
+        batch_actions     = []
+        batch_weights     = []
+        batch_returns     = []
+        batch_lens        = []
+        
+        observation = env.reset()
+        done = False
+        ep_rewards = []
+        
+        finished_rendering_this_epoch = False
+        
+        while True:
+            if (not finished_rendering_this_epoch) and render:
+                env.render()
+
+            batch_observation.append(observation.copy())
+            action = agent.choose_action(observation)
+            observation, reward, done, info = env.step(action)
+
+            batch_actions.append(action)
+            ep_rewards.append(reward)
+
+            if done:
+                ep_return, ep_length = sum(ep_rewards), len(ep_rewards)
+                batch_returns.append(ep_return)
+                batch_lens.append(ep_length)
+                rtg_array = [0 for i in range(ep_length)]
+                for j in range(ep_length-2, -1, -1):
+                    rtg_array[j] = rtg_array[j+1] + ep_rewards[j+1]
+                batch_weights += rtg_array
+
+                observation, done, ep_rewards = env.reset(), False, []
+
+                finished_rendering_this_epoch = True
+                if len(batch_observation) > batch_size:
+                        break
+        
+        batch_loss = agent.learn(batch_states=batch_observation, batch_actions=batch_actions, batch_weights=batch_weights)
+
+        avg_score = np.mean(batch_returns)
+        print("Epoch number: {}, Loss : {}, Average_returns: {}".format(i, batch_loss.item(), avg_score))
+        score_history.append(avg_score)
+
+        if avg_score > best_score:
+            best_score = avg_score
+            if not load_checkpoint:
+                agent.save_models()
+
+    games = [i for i in range(n_games)]
+    plt.plot(games, score_history)
+    plt.title("Score plot")
+    plt.xlabel("Game number")
+    plt.ylabel("Score")
+    plt.savefig("results/plot.png")
